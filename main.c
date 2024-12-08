4a4438835a38f4a8ed5da020ccbc3711f34c64e2
Omar-Goba <omaryasserassem1@gmail.com>
2024-12-08 19:48:45 +0200

    feat: add gyroscope sensor integration
    - Added `gyroscope.h` header file with definitions and function
      declarations for gyroscope sensor.
    - Implemented `gyroscope.c` with functions to initialize I2C, read
      accelerometer and gyroscope data, and calculate angular displacement.
    - Updated `CMakeLists.txt` to include `hardware_i2c` library.
    - Modified `main.c` to include gyroscope sensor setup and task creation.

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include "FreeRTOS.h"
+#include "hardware/uart.h"
 #include "pico/stdlib.h"
 #include "task.h"
 
 #include "src/actuators/led.c"
 #include "src/actuators/motor.c"
+#include "src/sensors/gyroscope.c"
 
 int main() {
   // ~~~ SETUP PICO ~~~ //
@@ -13,8 +15,11 @@ int main() {
   led_setup();
   motor_setup();
 
+  // ~~~ TASKS ~~~ //
   xTaskCreate(led_loop, "LED_Task", 256, NULL, 1, NULL);
   xTaskCreate(motor_loop, "Motor_Task", 256, NULL, 1, NULL);
+
+  // ~~~ START SCHEDULER ~~~ //
   vTaskStartScheduler();
 
   while (1) {
